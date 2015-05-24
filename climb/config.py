35ecc097ff8677a9990a49876520fe32094d8d80
m110 <m110@m110.pl>
2015-05-24 12:13:47 +0200

    Add method for direct config loading.

--- climb/config.py
+++ climb/config.py
@@ -17,9 +17,23 @@ def load_config(name):
 
     paths = [path.format(name=name) for path in DEFAULT_CONFIG_PATHS]
     for config_path in paths:
-        config_path = os.path.expanduser(config_path)
-        if os.path.isfile(config_path) and os.access(config_path, os.R_OK):
-            config.read(config_path)
+        if _read_config(config_path):
             break
     else:
         raise ConfigNotFound("Could not find {name}.conf".format(name))
+
+
+def load_config_file(path):
+    config.clear()
+
+    if not _read_config(path):
+        raise ConfigNotFound("Could not load {}".format(path))
+
+
+def _read_config(path):
+    config_path = os.path.expanduser(path)
+    if os.path.isfile(config_path) and os.access(config_path, os.R_OK):
+        config.read(config_path)
+        return True
+    else:
+        return False
